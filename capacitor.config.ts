edb6bc70f0e089dc5347ff8359581402e86685ae
David <daalvarez@birt.eus>
2024-02-05 21:20:01 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alvarez_martinez_david_DI04_TaEv01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
